b08b607c96e23b371283534dcfbf258e5277d54f
dounyaa <78869241+dounyaa@users.noreply.github.com>
2022-04-07 18:52:19 +0200

    Update AddNewTask.java

--- app/src/main/java/com/example/todo/AddNewTask.java
+++ app/src/main/java/com/example/todo/AddNewTask.java
@@ -96,6 +96,7 @@ public class AddNewTask extends BottomSheetDialogFragment {
                     ToDoModel task = new ToDoModel();
                     task.setTask(text);
                     task.setStatus(0);
+                    db.insertTask(task);
                 }
                 dismiss();
             }
